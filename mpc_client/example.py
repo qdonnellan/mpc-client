9b436ade37c5229897969402ec25a128610ea187
Quentin Donnellan <quentin@spideroak.com>
2015-10-02 08:40:35 -0500

    removed old example file

--- mpc_client/example.py
+++
@@ -1,8 +0,0 @@
-"""An example query using the Query class"""
-from .query import Query
-from astropy import units as u
-
-
-ceres = Query().filter(name='Ceres').limit(1).first()
-print ceres.orbit.semimajor_axis
-print ceres.orbit.semimajor_axis + (1000 * u.km)
